ee663dec13b9e2cf8b891983e5f4e7db494000b1
galathinius <anisoara.ionela@gmail.com>
2021-11-10 19:54:31 -0700

    t1: it works!!!

--- assignment_3/t1s/Ranking.scala
+++ assignment_3/t1s/Ranking.scala
@@ -6,23 +6,23 @@ object Ranking {
 
   def main(args: Array[String]): Unit = {
 
-    val sc = SparkSession.builder().master("spark://salt-lake-city:30356").getOrCreate().sparkContext
+    val sc = SparkSession.builder().master("spark://salem:30356").getOrCreate().sparkContext
 
 
     // val text = sc.textFile(args(0))
     val lines = sc.textFile(args(0), 10)
-    val titles = sc.textFile(args(1),10).zipWithIndex().mapValues(x => x + 1)//.map(_.swap)
+    val titles = sc.textFile(args(1),10).zipWithIndex().mapValues(x => x + 1).map(_.swap)
     val numOfPages = lines.count()
-    val links = lines.map(s=>(s.split(": ")(0), s.split(": ")(1)))
+    val links = lines.map(s=>(s.split(": ")(0).toLong, s.split(": ")(1)))
     var ranks = links.mapValues(v => 1.0 / numOfPages)  
 
     // ranks.saveAsTextFile(args(1))
 
-    for ( i <- 1 to 2) {
+    for ( i <- 1 to 25) {
         val tempRank = links.join(ranks, 10).values.flatMap {  
             case (urls, rank) =>
             val outgoingLinks = urls.split(" ")
-            outgoingLinks.map(url => (url, rank / outgoingLinks.length))
+            outgoingLinks.map(url => (url.toLong, rank / outgoingLinks.length))
         }
         // Updated ranks: { (B, _), (C, _), (D, _), (A, _), ... }  
         ranks = tempRank.reduceByKey(_+_)
@@ -31,14 +31,21 @@ object Ranking {
     val sortedRanks = ranks.sortBy(_._2, false).take(10)
     val rddRanks = sc.parallelize(sortedRanks)
     
+  // ('a', (1, 2))    common key, left value, right value
 
+    val titleRank = rddRanks.join(titles, 10)
+    
+    // println(rddRanks.getClass)
 
-    val tempRank = titles.join(rddRanks, 10).values.flatMap {  
-            case (id, rank) =>
-            val outgoingValue = id +" "+ rank
-            titles.map(title => (title, outgoingValue ))
+    
+    val forOutput = titleRank.map {
+        // .map (rec =>(rec._2._2, rec._2._1)) 
+            rec => rec._2._2 + ' ' + rec._2._1 + ' ' + rec._1
+            // val outgoingValue = id +" "+ rank
+            // titles.map(title => (title, outgoingValue ))
         }
-       titles.saveAsTextFile(args(2))
+       forOutput.saveAsTextFile(args(2))
+
     // println(ranks.getClass)
     // val counts = text.flatMap(line => line.split(" ")
     // ).map(word => (word,1)).reduceByKey(_+_)

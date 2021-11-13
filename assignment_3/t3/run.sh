aa68027c195fa16e63c1bf48442468f8bc4e8e11
galathinius <anisoara.ionela@gmail.com>
2021-11-12 20:05:19 -0700

    add t3 run.sh

---
+++ assignment_3/t3/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "Compiling the program..."
+# Compile the program
+sbt package
+echo "Deleting output directory..."
+# Delete the output directory
+$HADOOP_HOME/bin/hadoop fs -rm -r /outReal3
+echo "Running the program..."
+# Run the program...
+
+# /inputReal/links-simple-sorted.txt /inputReal/titles-sorted.txt /outReal 
+#$SPARK_HOME/bin/spark-submit --class RankingTaxation --deploy-mode client --supervise target/scala-2.12/simple-project_2.12-1.0.jar /input/testInput.txt /input/testTitles.txt /outTest 
+$SPARK_HOME/bin/spark-submit --class RankingBomb --deploy-mode client --supervise target/scala-2.12/simple-project_2.12-1.0.jar /input-mod/links-modified.txt /input-mod/titles-modified.txt /outReal3 
+
+echo "Showing the output ..."
+$HADOOP_HOME/bin/hadoop fs -cat /outReal3/*
